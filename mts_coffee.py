a98e71e26b6e4894188731f9e5dad3b2ae858da2
Christopher Smith <christopherj.smith@outlook.com>
2019-01-14 23:01:49 -0800

    refactored main scrape into small functions

--- mts_coffee.py
+++ mts_coffee.py
@@ -25,47 +25,63 @@ class Coffee(BaseModel):
 def cash_int(price):
     price_float = float(price)
     return int(price_float * 100)
+    
+def check_matchstick(item):
+    if item['product_type'] == '3 x 3/4 lb bags' or item['title'] in ['Catalogue','The Bulldog','Decaf']:
+        return True
+    else:
+        return False
+
+def get_matchstick():
+    try:
+        matchstick = Roaster.create(roaster='Matchstick',
+                             country='Canada',
+                             region='British Columbia',
+                             city='Vancouver')
+    except:
+        print('already made matchstick')
+    finally:
+        r = requests.get('https://matchstickyvr.com/collections/coffee/products.json')
+        r_json = r.json()
+        coffees = r_json['products']
+        output = [coffee for coffee in coffees if not check_matchstick(coffee)]
+        return matchstick, output
+
+def read_desc(desc):
+    desc_html = BeautifulSoup(desc, 'html.parser')
+    desc_dict = dict()
+    items = desc_html.find_all('p')
+    for i in range(len(items)):
+        if i == 1:
+            continue
+        attribs = items[i].find_all('strong')
+        for attrib in attribs:
+            if attrib.nextSibling == None or attrib.nextSibling.string == None:
+                pairs = attrib.get_text().split(":")
+                desc_dict[pairs[0].strip()] = pairs[1].strip()
+            else:
+                desc_dict[attrib.get_text().strip().replace(':','')] = attrib.nextSibling.string.replace('\xa0', ' ').strip()
+    return desc_dict
 
 def matchstick_scrape():
-    matchstick = Roaster.create(roaster='Matchstick',
-                         country='Canada',
-                         region='British Columbia',
-                         city='Vancouver')
-    print(matchstick.roaster)
-    r = requests.get('https://matchstickyvr.com/collections/coffee/products.json')
-    r_json = r.json()
-    coffees = r_json['products']
-    coffee_data = []
+    matchstick, coffees = get_matchstick()
     for coffee in coffees:
-        if coffee['product_type'] == '3 x 3/4 lb bags' or coffee['title'] in ['Catalogue','The Bulldog','Decaf']:
-            continue
-        else:
-            name = coffee['title']
-            country = coffee['product_type']
-            price = cash_int(coffee['variants'][0]['price'])
-            desc_html = BeautifulSoup(coffee['body_html'], 'html.parser')
-            desc_dict = dict()
-            items = desc_html.find_all('p')
-            for i in range(len(items)):
-                if i == 1:
-                    continue
-                attribs = items[i].find_all('strong')
-                for attrib in attribs:
-                    if attrib.nextSibling == None or attrib.nextSibling.string == None:
-                        pairs = attrib.get_text().split(":")
-                        desc_dict[pairs[0].strip()] = pairs[1].strip()
-                    else:
-                        desc_dict[attrib.get_text().strip().replace(':','')] = attrib.nextSibling.string.replace('\xa0', ' ').strip()
-            coffee_store = { 'coffee':name,
-                             'price':price,
-                             'country':country,
-                             'region':desc_dict['Region'],
-                             'process':desc_dict['Process'],
-                             'tasting_notes':desc_dict['Tasting Notes'],
-                             'roaster':matchstick }
-            coffee_data.append(coffee_store)
+        name = coffee['title']
+        country = coffee['product_type']
+        price = cash_int(coffee['variants'][0]['price'])
+        desc_dict = read_desc(coffee['body_html'])
+        coffee_store = { 'coffee':name,
+                         'price':price,
+                         'country':country,
+                         'region':desc_dict['Region'],
+                         'process':desc_dict['Process'],
+                         'tasting_notes':desc_dict['Tasting Notes'],
+                         'roaster':matchstick }
+        coffee_data.append(coffee_store)
     Coffee.insert_many(coffee_data).execute()
 
-database.create_tables([Roaster, Coffee])
-print('created tables')
-matchstick_scrape()
+# database.create_tables([Roaster, Coffee])
+# print('created tables')
+# matchstick_scrape()
+
+get_matchstick()
